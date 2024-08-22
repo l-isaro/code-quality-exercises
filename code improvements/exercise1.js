53f6876c90537b87c278401e2ffd0d3aaaa41e62
l-isaro <l.isaro@alustudent.com>
2024-08-22 19:27:27 +0200

    Removed extra spacing and unnecessary variables

--- code improvements/exercise1.js
+++ code improvements/exercise1.js
@@ -1,9 +1,5 @@
 function calculateAverage(numbers) {
   const sum = numbers.reduce((a, b) => a + b, 0);
-
   const count = numbers.length;
-
-  const average = sum / count;
-
-  return average;
+  return sum / count;
 }
