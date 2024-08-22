c495b349d2ce0cdc9ed733dc7ec0c1fa08dc2772
l-isaro <l.isaro@alustudent.com>
2024-08-22 10:33:20 +0200

    exercise 9

---
+++ exercise9.js
@@ -0,0 +1,9 @@
+function calculateMatrixSum(matrix) {
+  let sum = 0;
+  const rowLength = matrix.length;
+  const columnLength = matrix[0].length;
+  for (let row = 0; row < rowLength; row++)
+    for (let column = 0; column < columnLength; column++)
+      sum += matrix[row][column];
+  return sum;
+}
