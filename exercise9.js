7c09fbee32450e0ba4ede6071a2691a506d76a45
l-isaro <l.isaro@alustudent.com>
2024-08-23 11:20:37 +0200

    Refactored function to use array methods

--- exercise9.js
+++ exercise9.js
@@ -1,11 +1,5 @@
 function calculateMatrixSum(matrix) {
-  let sum = 0;
-  const rowLength = matrix.length;
-  const columnLength = matrix[0].length;
-
-  for (let row = 0; row < rowLength; row++)
-    for (let column = 0; column < columnLength; column++)
-      sum += matrix[row][column];
-
-  return sum;
+  return matrix
+    .map((row) => row.reduce((sum, value) => sum + value, 0))
+    .reduce((totalSum, rowSum) => totalSum + rowSum, 0);
 }
